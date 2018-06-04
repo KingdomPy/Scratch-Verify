20b5846b01170bf08e55a3e7b240e2c06cc77499
unknown <nathanessel1@gmail.com>
2018-06-04 19:30:22 +0100

    Initial commit

---
+++ worker.py
@@ -0,0 +1,52 @@
+#Scratch Search Tools
+import requests
+import re
+#Discord API
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord.utils import get
+import asyncio
+
+def getCode(user, code):
+    page = requests.get("https://scratch.mit.edu/site-api/comments/user/"+user+"/?page=1")
+    cleanr = re.compile('<.*?>')
+    cleantext = re.sub(cleanr, '', page.text)
+    lines = cleantext.split()
+    comments = [line for line in cleantext.split('\n') if line.strip() != '']
+    for i in range(0, len(comments)):
+        comments[i] = " ".join(comments[i].split())
+    for i in range(0, len(comments)):
+        if comments[i] == user:
+            if comments[i+1] == code:
+                return True
+                break
+                
+    return False
+
+Client = discord.Client()
+client = commands.Bot(command_prefix="%")
+
+@client.event
+async def on_ready():
+    print("Bot is ready")
+
+
+@client.event
+async def on_message(message):
+    if message.content == "%verifyme":
+        userID = message.author.id
+        await client.send_message(message.channel, ("Please post the code {} on the profile you want to verify on, then type %verify (username)").format(userID))
+
+    if message.content.lower().startswith("%verify"):
+        userID = message.author.id
+        args = " ".join(message.content.split(" ")[1:])
+        if args != "":
+            if getCode(args, userID):
+                role = get(message.server.roles, name='Verified')
+                await client.add_roles(message.author, role)
+                await client.send_message(message.channel, "Authentication successfully completed")
+            else:
+                await client.send_message(message.channel, "Authentication failed, please try again")
+        
+client.run("NDUyODUyNjA4NDI4NjcwOTg2.DfWZpg.bxQeETNk-BAHaG3aqJKORhOOONY")
